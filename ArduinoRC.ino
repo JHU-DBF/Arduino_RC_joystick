2a056fd83b78efd53a52f0c622de38a38b1daa45
Koji Shukawa <me@koji.space>
2024-05-10 23:15:59 -0400

    Initial commit

---
+++ ArduinoRC.ino
@@ -0,0 +1,139 @@
+// #include "MegaJoy.h"
+#include "UnoJoy.h"
+
+#define CH1 21
+#define CH2 20
+#define CH3 19
+#define CH4 18
+#define CH5 2
+
+#define NEUTRAL_THROTTLE 1000
+#define NEUTRAL_INPUT 1500
+
+volatile int nCH1In = NEUTRAL_THROTTLE;
+volatile int nCH2In = NEUTRAL_INPUT;
+volatile int nCH3In = NEUTRAL_INPUT;
+volatile int nCH4In = NEUTRAL_INPUT;
+volatile int nCH5In = NEUTRAL_INPUT;
+
+volatile unsigned long ulStartPeriod1 = 0;
+volatile unsigned long ulStartPeriod2 = 0;
+volatile unsigned long ulStartPeriod3 = 0;
+volatile unsigned long ulStartPeriod4 = 0;
+volatile unsigned long ulStartPeriod5 = 0;
+
+void setup(){
+  attachInterrupt(digitalPinToInterrupt(CH1),calcInput1,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CH2),calcInput2,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CH3),calcInput3,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CH4),calcInput4,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(CH5),calcInput5,CHANGE);
+
+  // setupMegaJoy();
+  setupUnoJoy();
+}
+
+void loop(){
+  // megaJoyControllerData_t controllerData = getControllerData();
+  dataForController_t controllerData = getControllerData();
+  setControllerData(controllerData);
+}
+
+
+
+dataForController_t getControllerData(void){
+// megaJoyControllerData_t getControllerData(void){
+
+  dataForController_t controllerData =  getBlankDataForController();
+  // megaJoyControllerData_t controllerData = getBlankDataForMegaController();
+  // for (int i = 2; i < 54; i++){
+  //   controllerData.buttonArray[(i - 2) / 8] |= (LOW) << ((i - 2) % 8);
+  // }
+  
+  controllerData.leftStickX = (int)((nCH4In - 1000)*0.256);
+  controllerData.leftStickY = (int)((nCH1In - 1000)*0.256);
+  controllerData.rightStickX = (int)((nCH2In - 1000)*0.256);
+  controllerData.rightStickY = (int)((nCH3In - 1000)*0.256);
+
+  return controllerData;
+}
+
+void calcInput1()
+{
+  if(digitalRead(CH1) == HIGH)
+  { 
+    ulStartPeriod1 = micros();
+  }
+  else
+  {
+    if(ulStartPeriod1)
+    {
+      nCH1In = (int)(micros() - ulStartPeriod1);
+      ulStartPeriod1 = 0;
+    }
+  }
+}
+
+void calcInput2()
+{
+  if(digitalRead(CH2) == HIGH)
+  { 
+    ulStartPeriod2 = micros();
+  }
+  else
+  {
+    if(ulStartPeriod2)
+    {
+      nCH2In = (int)(micros() - ulStartPeriod2);
+      ulStartPeriod2 = 0;
+    }
+  }
+}
+
+void calcInput3()
+{
+  if(digitalRead(CH3) == HIGH)
+  { 
+    ulStartPeriod3 = micros();
+  }
+  else
+  {
+    if(ulStartPeriod3)
+    {
+      nCH3In = (int)(micros() - ulStartPeriod3);
+      ulStartPeriod3 = 0;
+    }
+  }
+}
+
+void calcInput4()
+{
+  if(digitalRead(CH4) == HIGH)
+  { 
+    ulStartPeriod4 = micros();
+  }
+  else
+  {
+    if(ulStartPeriod4)
+    {
+      nCH4In = (int)(micros() - ulStartPeriod4);
+      ulStartPeriod4 = 0;
+    }
+  }
+}
+
+void calcInput5()
+{
+  if(digitalRead(CH5) == HIGH)
+  { 
+    ulStartPeriod5 = micros();
+  }
+  else
+  {
+    if(ulStartPeriod5)
+    {
+      nCH5In = (int)(micros() - ulStartPeriod5);
+      ulStartPeriod5 = 0;
+    }
+  }
+}
---
+++ READ.md
@@ -0,0 +1,30 @@
+## Connect RC controller to your computer
+Requires an Arduino Mega and any RC receivers
+
+### Reccomended RC simulators
+This code makes Arudino+RC controller emulates USB joystick for any computer and software.
+
+For windows: Phoenix RC [Download](https://www.rc-thoughts.com/phoenix-sim/)
+do not forget to install an emulator to avoid needing their proprietary dongle.
+
+For Linux: Check this website [here](https://www.linuxquestions.org/questions/linux-games-33/linux-rc-simulators-4175668078/). Koji and Serzed have tried [crrcsim](https://sourceforge.net/projects/crrcsim/). Not as realistic as Phoenix RC, but it works. You can install it via apt and ARU (yay).
+
+
+### Wiring
+Connect RC receiver's ground to Arduino's GND
+power (middle pin) to 5V on Arduino
+Connect signal to Arudino pins according to the table below
+| RC Channel | Arduino Pin |
+| - | -- |
+| 1 | 21 |
+| 2 | 20 |
+| 3 | 19 |
+| 4 | 18 |
+
+### How to install
+1. se Arudino IDE to upload this code to Arduino Mega
+2. Download [UnoJoy](https://github.com/AlanChatham/UnoJoy)
+3. Put Arduino into DFU MODE ([guide](https://support.arduino.cc/hc/en-us/articles/4410804625682-Set-a-board-to-DFU-mode))
+4. Run Unojoy/TurnIntoAJoystick (.bat in windows, .command in Mac?, .sh in Linux) to turn Arduino into a USB joystick
+
+When you want to turn the Arduino back into normal Arduino, run MegaJoy/TurnIntoAnArduino
